765d6d334588a105e1731dbd75f3371ae1370449
Nam Thanh Le <lethanhnam2882@gmail.com>
2018-10-16 01:31:05 +0700

    Fix root tag error code. Resolver now will skip XML Declaration when splitting. Resolver now can understand CDATA and will ignore CDATA in tag.

--- src/trinity/sample/library/NestedTagResolver.java
+++ src/trinity/sample/library/NestedTagResolver.java
@@ -33,6 +33,11 @@ public class NestedTagResolver {
         int tmp;
         String s;
         boolean isComment = false;
+        boolean isXMLDeclaration = false;
+        boolean isCharacterData = false;
+        boolean isInTag = false;
+        int xmlDeclarationStack = 0;
+        int cdataStack = 0;
         while (true) {
             try {
                 tmp = reader.read();
@@ -44,6 +49,7 @@ public class NestedTagResolver {
                     if (tmp == '-') {
                         reader.mark(2);
                         int c = reader.read();
+                        //[case]: end of comment
                         if (c == '-' && (c = reader.read()) == '>') {
                             isComment = false;
                             continue;
@@ -56,29 +62,112 @@ public class NestedTagResolver {
                 if (tmp == '<') {
                     reader.mark(3);
                     int c = reader.read();
-                    if (c == '!' && (c = reader.read()) == '-' && (c = reader.read()) == '-') {
-                        isComment = true;
-                        continue;
+                    if (c == '!') {
+                        if ((c = reader.read()) == '-' && (c = reader.read()) == '-') {
+                            //[case]: meet new comment
+                            isComment = true;
+                            continue;
+                        } else {
+                            //[case]: meet new xml declaration or CDATA
+                            //check if a CDATA
+                            if (!isXMLDeclaration) {
+                                reader.reset();
+                                reader.mark(7);
+                                reader.read();
+                                if ((c = reader.read()) == '[' && (c = reader.read()) == 'C'
+                                        && (c = reader.read()) == 'D' && (c = reader.read()) == 'A'
+                                        && (c = reader.read()) == 'T' && (c = reader.read()) == 'A') {
+                                    if (!isInTag) {
+                                        //[case]: meet a CDATA
+                                        isCharacterData = true;
+                                        reader.reset();
+                                    } else {
+                                        //[case]: skip CDATA in tag
+                                        cdataStack = 1;
+                                        do {
+                                            c = reader.read();
+                                            if (c == '>') {
+                                                cdataStack--;
+                                            } else if (c == '<') {
+                                                cdataStack++;
+                                            }
+                                        } while (c != -1 && cdataStack > 0);
+                                        continue;
+                                    }
+                                }
+                            }
+                            if (!isCharacterData) {
+                                //[case]: meet new xml declaration
+                                isXMLDeclaration = true;
+                                xmlDeclarationStack++;
+                                continue;
+                            }
+                        }
                     } else {
+                        //if not a xml declaration or comment
                         reader.reset();
-                    }
-                    if (localBuilder != null) {
-                        s = localBuilder.toString();
-                        if (s != null && !s.trim().equals("")) {
-                            rs.add(s);
+                        if (isXMLDeclaration) {
+                            //[case]: xml declaration tag is not closed
+                            xmlDeclarationStack = 0;
+                            isXMLDeclaration = false;
                         }
                     }
-                    localBuilder = new StringBuilder().append('<');
+                    if (localBuilder != null) {
+                        if (!isCharacterData) {
+                            //[case]: meet open tag
+                            s = localBuilder.toString();
+                            if (s != null && !s.trim().equals("")) {
+                                rs.add(s);
+                            }
+                            isInTag = true;
+                            localBuilder = new StringBuilder().append('<');
+                        } else {
+                            localBuilder.append('<');
+                            cdataStack++;
+                        }
+                    } else {
+                        //[case]: first time meet open tag
+                        isInTag = true;
+                        localBuilder = new StringBuilder().append('<');
+                    }
                 } else if (tmp == '>') {
+                    if (isXMLDeclaration) {
+                        xmlDeclarationStack--;
+                        if (xmlDeclarationStack == 0) {
+                            //[case]: xml declaration stack is empty
+                            isXMLDeclaration = false;
+                            continue;
+                        }
+                        if (xmlDeclarationStack > 0) {
+                            //[case]: xml declaration stack is not empty
+                            continue;
+                        }
+                    }
                     localBuilder.append('>');
+                    if (isCharacterData) {
+                        cdataStack--;
+                        if (cdataStack == 0) {
+                            isCharacterData = false;
+                        } else {
+                            continue;
+                        }
+                    }
+                    if (isInTag) {
+                        isInTag = false;
+                    }
                     rs.add(localBuilder.toString());
                     localBuilder = new StringBuilder();
-                } else if (localBuilder != null) {
+                } else if (localBuilder != null && !isXMLDeclaration) {
                     localBuilder.append((char) tmp);
                 }
             } catch (IOException ex) {
                 Logger.getLogger(NestedTagResolver.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
+        try {
+            reader.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NestedTagResolver.class.getName()).log(Level.SEVERE, null, ex);
         }
         return rs;
     }
@@ -90,8 +179,10 @@ public class NestedTagResolver {
                 return EMPTY_ELEMENT;
             } else if (s.charAt(1) == '/') {
                 return END_ELEMENT;
-            } else if (s.charAt(1) == '?' || s.charAt(1) == '!') {
+            } else if (s.charAt(1) == '?') {
                 return XML_DECLARATION;
+            } else if (s.charAt(1) == '!') {
+                return CHARACTERS;
             }
             return START_ELEMENT;
         } else {
@@ -165,9 +256,17 @@ public class NestedTagResolver {
         if (rootTag == null) {
             rootTag = "root";
         } else {
-            String root = formattedDoc.getFirst();
-            if (rootTag.equals(getLocalName(root))) {
-                addRootTag = false;
+            if (rootTag.equals("")) {
+                rootTag = "root";
+            }
+            try {
+                String start = formattedDoc.getFirst();
+                String end = formattedDoc.getLast();
+                if (checkTagType(start) == START_ELEMENT && checkTagType(end) == END_ELEMENT
+                        && rootTag.equals(getLocalName(start)) && rootTag.equals(getLocalName(end))) {
+                    addRootTag = false;
+                }
+            } catch (Exception e) {
             }
         }
         if (addRootTag) {
@@ -177,4 +276,39 @@ public class NestedTagResolver {
         return formattedDoc;
     }
 
+    public static void main(String[] args) {
+        String line = "<!DOCTYPE library[\n"
+                + "<!ELEMENT library (book)*>\n"
+                + "<!ELEMENT book (booktitle+, author, price)+>\n"
+                + "<!ELEMENT booktitle ( p | PCDATA)*>\n"
+                + "<!ELEMENT author (#PCDATA)>\n"
+                + "<!ELEMENT price (#PCDATA)>\n"
+                + "<!ELEMENT p (#PCDATA)>\n"
+                + "<!ATTLIST book id ID #REQUIRED>\n"
+                + "]>"
+                + "<!--[if IEMobile 7]><html class=\"iem7\" lang=\"vi\" dir=\"ltr\"><![endif]-->"
+                + "<xml><xm<![CDATA[<i>Comma</i>]]>l abc='<![CDATA[<i>Comma</i>]]>'>abc<a><![CDATA[<i>Comma</i>]]></a>xyz<![CDATA[<i>Comma</i>]]></xml><input>ksdhs</aaa>akjd<input>hskj<!--jhsadhjgdhgj<a>-->dhsakd<hbc/></xml>";
+        List<String> docStr = splitXmlDocument(line);
+        docStr.forEach(s -> {
+            int tagType = checkTagType(s);
+            switch (tagType) {
+                case START_ELEMENT:
+                    System.out.print("Start(" + getLocalName(s) + "): ");
+                    break;
+                case END_ELEMENT:
+                    System.out.print("End(" + getLocalName(s) + "): ");
+                    break;
+                case EMPTY_ELEMENT:
+                    System.out.print("Empty(" + getLocalName(s) + "): ");
+                    break;
+                case CHARACTERS:
+                    System.out.print("Chars: ");
+                    break;
+            }
+            System.out.println(s);
+        });
+        System.out.println("\nHandling------\n");
+        docStr = formatNestedTag(docStr, "xml");
+        docStr.forEach(System.out::println);
+    }
 }
